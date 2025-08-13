9756108d640fc38dbf1122e685592956e0282a47
Alec Cipkowski <=>
2025-08-13 16:58:00 -0400

    Set basename for App.jsx to allow for local and remote deployment Delete boilerplate readme text

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import { Application } from "@nmfs-radfish/react-radfish";
 import HomePage from "./pages/Home";
 
+// Define the basename conditionally using Vite's environment variables
+const basename = import.meta.env.DEV ? "/" : "/sari-app";
+
 function App() {
   return (
     <Application>
       <div className="App grid-container">
-        <Router>
+        <Router basename={basename}>
           <Routes>
             <Route path="/" element={<HomePage />} />
           </Routes>
